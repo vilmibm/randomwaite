0eed56ed71dd4b1e4caf169d5f0fae904d95b75f
nathaniel smith <nathanielksmith@gmail.com>
2016-09-29 23:04:09 -0700

    fair use

--- randomwaite/flickr.py
+++ randomwaite/flickr.py
@@ -5,28 +5,17 @@ import urllib.request
 
 from flickrapi.core import FlickrAPI
 
+FAIR_USE = True
 OK_LICENSES = [1,2,4,5,7]
-NO_ATTRIBUTION = [7]
 
 class Photo:
 
     def __init__(self, flickr: FlickrAPI, flickr_data: t.Dict) -> None:
         self.path = None
         self._img_data = None
-        self._license = None
         self._url = None
         self._data = flickr_data
         self.flickr = flickr
-
-    def needs_attribution(self) -> bool:
-        return self.license in NO_ATTRIBUTION
-
-    @property
-    def license(self) -> int:
-        if not self._license:
-            info = self.flickr.photos.getInfo(photo_id=self._data['id'], secret=self._data['secret'])
-            self._license = info['photo']['license']
-        return self._license
 
     @property
     def url(self) -> str:
@@ -34,10 +23,6 @@ class Photo:
             sizes = self.flickr.photos.getSizes(photo_id=self._data['id'])
             self._url = sizes['sizes']['size'][-1]['source']
         return self._url
-
-    @property
-    def attribution(self) -> str:
-        return "TODO"
 
     @property
     def data(self) -> BytesIO:  # TODO I don't think this return type is right
@@ -49,7 +34,10 @@ class Photo:
 
 def get_photo(flickr: FlickrAPI, search_term: str) -> Photo:
     # This will check title, description, and tags for the given word
-    result = flickr.photos.search(text=search_term, license=OK_LICENSES)
+    if FAIR_USE:
+        result = flickr.photos.search(text=search_term)
+    else:
+        result = flickr.photos.search(text=search_term, license=OK_LICENSES)
 
     # TODO pull from more than the first page
     photos = result['photos']['photo']
